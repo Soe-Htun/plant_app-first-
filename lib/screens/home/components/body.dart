7920df5b5869b8cc2f2e4c2b4a936aee86284577
soehtun <soehtuncu@gmail.com>
2021-05-14 13:46:33 +0630

    add text field

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:first/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
  class Body extends StatelessWidget {
    @override
@@ -23,11 +24,36 @@ import 'package:flutter/material.dart';
                   )),
                ),
                Positioned(
+                 bottom: 0,
+                 left: 0,
+                 right: 0,
                  child: Container(
+                   alignment: Alignment.center,
+                   
                    margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
                    height: 44,
                    decoration:  BoxDecoration(
-                     color: Colors.white
+                     color: Colors.white,
+                     borderRadius: BorderRadius.circular(20),
+                     boxShadow:  [
+                       BoxShadow(
+                         offset: Offset(0,10),
+                         blurRadius: 50,
+                         color: kPrimaryColor.withOpacity(0.23),
+                       )
+                     ]
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(
+                          color: kPrimaryColor.withOpacity(0.5),
+                        ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        suffixIcon: 
+                          SvgPicture.asset("graphics/icons/search.svg", color: kPrimaryColor, width: 25,)
+                      ),
                     ),
                  ) )
              ],),
--- lib/screens/home/components/home_screen.dart
+++ lib/screens/home/components/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:first/constants.dart';
 import 'package:first/screens/home/components/body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -18,7 +19,7 @@ class HomeScreen extends StatelessWidget{
     return AppBar(
       elevation: 0,
       leading: IconButton(
-        icon: SvgPicture.asset('graphics\icons\menu.svg') ,
+        icon: SvgPicture.asset('graphics/icons/menurepo.svg', color: Colors.white, width: 25,) ,
         onPressed: () {},
 
       ),
